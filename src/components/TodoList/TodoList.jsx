22d18ef1d5b083484d52eaa5653da88f9e3ee95d
Akash Gautam <akashgautam01584@gmail.com>
2024-07-02 19:47:56 +0530

    simplify code

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,6 +5,31 @@ import TodoContext from "../../context/TodoContext";
 function TodoList() {
 	const { list, setList } = useContext(TodoContext);
 
+	function onFinished(todo, isFinished) {
+		const updatedList = list.map((t) => {
+			if (t.id == todo.id) {
+				todo.finished = isFinished;
+			}
+			return t;
+		});
+		setList(updatedList);
+	}
+
+	function onDelete(todo) {
+		const updatedList = list.filter((t) => t.id != todo.id);
+		setList(updatedList);
+	}
+
+	function oneEdit(todo, todoText) {
+		const updatedList = list.map((t) => {
+			if (t.id == todo.id) {
+				todo.todoData = todoText;
+			}
+			return t;
+		});
+		setList(updatedList);
+	}
+
 	return (
 		<div
 			style={{
@@ -13,37 +38,18 @@ function TodoList() {
 				gap: "12px",
 			}}
 		>
-			{list.length > 0 &&
+			{list.length >= 0 &&
 				list.map((todo) => (
 					<Todo
 						key={todo.id}
 						id={todo.id}
 						todoData={todo.todoData}
 						isFinished={todo.finished}
-						changeFinished={(isFinished) => {
-							const updatedList = list.map((t) => {
-								if (t.id == todo.id) {
-									todo.finished = isFinished;
-								}
-								return t;
-							});
-							setList(updatedList);
-						}}
-						onDelete={() => {
-							const updatedList = list.filter(
-								(t) => t.id != todo.id
-							);
-							setList(updatedList);
-						}}
-						onEdit={(todoText) => {
-							const updatedList = list.map((t) => {
-								if (t.id == todo.id) {
-									todo.todoData = todoText;
-								}
-								return t;
-							});
-							setList(updatedList);
-						}}
+						changeFinished={(isFinished) =>
+							onFinished(todo, isFinished)
+						}
+						onDelete={() => onDelete(todo)}
+						onEdit={(todoText) => oneEdit(todo, todoText)}
 					/>
 				))}
 		</div>
